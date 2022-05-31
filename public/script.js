6cc047f02c5f6ac82fe329b6b74f64d5daed8306
Matthew Cockcroft <94088344+RearedCape8319@users.noreply.github.com>
2022-05-31 08:28:54 +0100

    Created database management

---
+++ public/script.js
@@ -0,0 +1,30 @@
+async function enterWorkout() {
+    let target = document.getElementById("target").value;
+    let name = document.getElementById("name").value;
+    let sets = document.getElementById("sets").value;
+    let reps = document.getElementById("reps").value;
+    let options = {
+        method: "post",
+        body: JSON.stringify({ target, name, sets, reps }),
+        headers: { "Content-Type": "application/json" }
+    };
+    const response = await fetch("/api/storeWorkout", options);
+    const data = await response.json();
+    console.log(data);
+}
+
+async function getAllWorkouts() {
+    let options = {
+        method: "get",
+        headers: { "Content-Type": "application/json" }
+    };
+    const response = await fetch("/api/allWorkouts", options);
+    const data = await response.json();
+    console.log(data);
+    let host = document.getElementById("all");
+    for (let w of data) {
+        let p = document.createElement("p");
+        p.textContent = `${w.target}, ${w.name}, ${w.sets}x${w.reps}`;
+        host.appendChild(p);
+    }
+}
--- server.js
+++ server.js
@@ -7,29 +7,36 @@ app.use(
     })
 );
 app.use(express.json());
+app.use(express.static("public"));
 
 // Use the database for workouts
 const Datastore = require("nedb");
-const workouts = new Datastore({ filename: "data/workouts.db" });
-workouts.loadDatabase((err) => {
-    if (err != null) {
-        console.log("Error loading database!")
-    }
-});
-
+const workouts = new Datastore({ filename: "data/workouts.db", autoload: true });
 
 
 
 // Define get and post requests for database management
 app.get("/api/allWorkouts", (request, response) => {
     console.log("Giving all workouts");
-    response.json({ message: "All workouts" });
+    workouts.find({}).sort({ target: 1 }).exec((err, docs) => {
+        if (err != null) {
+            console.log("Error retrieving all workouts");
+        } else {
+            response.json(docs);
+        }
+    });
 });
 
 app.post("/api/storeWorkout", (request, response) => {
-    console.log("Storing workout");
-    console.log(request.body);
-    response.json({ message: "Stored workout" });
+    let data = request.body;
+    workouts.insert(data, (err, newDoc) => {
+        if (err != null) {
+            console.log("Error storing to database!", err);
+        } else {
+            console.log("Stored new workout!", newDoc);
+            response.json({ message: "Success!", record: newDoc });
+        }
+    });
 });
 
 
